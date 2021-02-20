405264c103f9d2a225ecdc64c6c266d85ff3455c
Hiro <hiroyuki1126@gmail.com>
2021-02-20 17:29:05 +0900

    コンテナ名変更に伴う修正。

--- g_user_make.php
+++ g_user_make.php
@@ -1,5 +1,5 @@
 <?php
-$db = new PDO("mysql:host=dev-db;dbname=image-board", "root", "root");
+$db = new PDO("mysql:host=dev-php-db;dbname=image-board", "root", "root");
 $db->query("INSERT INTO table2 (id, pas, nam) VALUES ('admin', md5('himitunopass'), 'nisizawa')");
 $db->query("INSERT INTO table2 (id, pas, nam) VALUES ('user1', md5('inupass'), 'dog')");
 $db->query("INSERT INTO table2 (id, pas, nam) VALUES ('user2', md5('nekopass'), 'cat')");
